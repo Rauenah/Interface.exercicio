4494bf213ea3f2f736ea9b7737d12c8160c3093a
rauenah <rauena.silvasousa@gmail.com>
2022-07-19 00:06:55 -0300

    Interface excercicio.

---
+++ src/dio/com/OperaçãoMatematica.java
@@ -0,0 +1,13 @@
+package dio.com;
+
+/**Interface de exemplo para o exercicio Interface da aula Programação Orientada a Objeto*/
+
+interface OperacaoMatematica {
+    void soma(double operando1, double operando2);
+
+    void subtracao(double operando1, double operando2);
+
+    void multiplicacao (double operando1, double operando2);
+
+    void divisao(double operando1, double operando2);
+}
